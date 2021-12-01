87e91efd13e85596032ced8b956cff40412a1bc0
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-11-30 21:12:32 -0600

    update api file

---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const Workout = require("../models/workouts");
+
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+});
+
+router.post("/api/workout/bulk", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+});
+
+router.get("/api/workout", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+});
+
+module.exports = router;
