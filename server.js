3849b9c8ef6c03068fa29fac282cb75b56a56eab
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-12-03 01:56:51 -0600

    added html routes file

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose.connect("mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
