514ca0eca077e6e3d4ddd80e09ec941d99d199cd
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-12-03 02:42:40 -0600

    added workout/id route

--- routes/api.js
+++ routes/api.js
@@ -22,6 +22,22 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.put("/api/workouts/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  db.Workout.findOneAndUpdate(
+    { _id: id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(404).json(err);
+    });
+});
+
 router.get("/api/workouts/range", (req, res) => {
   db.Workout.find({})
     .sort({ date: -1 })
